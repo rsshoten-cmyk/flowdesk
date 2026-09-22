fe743f65e3874b1f018cc2800193d599af526c1a
rsshoten-cmyk <r.s.shoten@gmail.com>
2026-09-22 15:40:08 +0900

    Build shared employee calendar

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'flowdesk-v2';
+const CACHE = 'flowdesk-v4';
 const FILES = ['./', './業務管理アプリ.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));

0feba577ce6fd43b0f48484c30951b874733eb71
rsshoten-cmyk <r.s.shoten@gmail.com>
2026-09-22 14:30:28 +0900

    Deploy FlowDesk: add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+const CACHE = 'flowdesk-v2';
+const FILES = ['./', './業務管理アプリ.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+
